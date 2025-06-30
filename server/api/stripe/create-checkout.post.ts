d323f6cc4c5155f367e2d5e1647adc2d5a2268c2
katokdoescode <47973467+katokdoescode@users.noreply.github.com>
2025-06-30 15:26:47 +0400

    Expose error

--- server/api/stripe/create-checkout.post.ts
+++ server/api/stripe/create-checkout.post.ts
@@ -107,6 +107,7 @@ export default defineEventHandler(async (event) => {
 		throw createError({
 			statusCode: 500,
 			statusMessage: 'Failed to create checkout session',
+			data: error,
 		});
 	}
 });
